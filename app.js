40953d20f60fe3c68123b69f8f910ae42dd14807
navya.nucchu@gmail.com <navya.nucchu@gmail.com>
2022-07-16 20:31:34 -0500

    node 16 version

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express();
+
+const path = require('path');
+const port = 5000;
+
+app.listen(port, (err) => {
+    if(err) return console.log(err);
+    console.log('Server running on port', port);
+})
