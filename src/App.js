9f728b43048084c36cbd1b2d77354114313ff564
navya.nucchu@gmail.com <navya.nucchu@gmail.com>
2022-07-19 12:39:28 -0500

    Add Env and end point to get data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import axios from "axios";
 import baseUrl from './config';
 const App = () => {
+    console.log(baseUrl);
     axios(`${baseUrl}/api/v1/books`).then(books => {
         console.log(books);
     })
